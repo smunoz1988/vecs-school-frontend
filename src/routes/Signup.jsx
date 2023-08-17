137fa1fd4637d06f9f1beba717646f8cf606e0e9
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-17 17:55:41 -0500

    add form to sign up

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
 const Signup = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>Signup</div>
+    <>
+      <p onClick={() => navigate('/')}>Back</p>
+      <div>Sign Up</div>
+      <form> {/* in here should be the onclick to call to reducer to manage logic */}
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <div>
+        <span>Do you already have an account? </span>
+        <Link to="/Signin">Create an account</Link>
+      </div>
+    </>
   )
-}
+};
 
 export default Signup
