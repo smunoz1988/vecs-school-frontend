8847eacd1788991fbbf9c37e4714594adfb1265d
vvoo21 <vaneolipa@gmail.com>
2023-08-18 13:36:20 -0500

    Add route for Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Courses from './routes/Courses';
+import Details from './routes/Details';
 import Reserve from './routes/Reserve';
 import Reservations from './routes/Reservations';
 import AddCourse from './routes/AddCourse';
 import DeleteCourse from './routes/DeleteCourse';
 import Signin from './routes/Signin';
 import Signup from './routes/Signup';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/Courses" element={<Courses />} />
+        <Route path="/Details" element={<Details />} />
         <Route path="/Reserve" element={<Reserve />} />
         <Route path="/Reservations" element={<Reservations />} />
         <Route path="/AddCourse" element={<AddCourse />} />
---
+++ src/routes/Details.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+
+const Details = () => {
+  return (
+    <div class="flex">
+      <Navbar />
+      <h1>Details</h1>
+    </div>
+  )
+}
+
+export default Details
