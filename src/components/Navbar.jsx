610c98dcb9b5070c7054e3ba0d368cf3cd7931e4
vvoo21 <vaneolipa@gmail.com>
2023-08-18 13:37:29 -0500

    Create the basic structure for Courses and Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,67 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { NavLink } from 'react-router-dom';
 
-export default Navbar
+const Navbar = () => {
+  const activeStyle = {
+    textDecoration: 'underline',
+  };
+  const activeClassName = 'underline';
+  return (
+    <nav>
+      <a href="#/">
+        <h1 class="font_Caveat">VECS SCHOOL</h1>
+      </a>
+      <ul className="nav-links">
+        <li>
+          <NavLink
+            to="/"
+            end
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Courses
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Reserve"
+            className={({ isActive }) =>
+              isActive ? activeClassName : undefined
+            }
+          >
+            Reserve
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Reservations"
+            className={({ isActive }) =>
+              isActive ? activeClassName : undefined
+            }
+          >
+            My Reservations
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/AddCourse"
+            className={({ isActive }) =>
+              isActive ? activeClassName : undefined
+            }
+          >
+            Add Course
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/DeleteCourse"
+            className={({ isActive }) =>
+              isActive ? activeClassName : undefined
+            }
+          >
+            Delete Course
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
--- src/routes/Courses.jsx
+++ src/routes/Courses.jsx
@@ -1,6 +1,69 @@
+import Navbar from "../components/Navbar"
+import { useState } from 'react';
+import Slider from "../components/Slider";
+
+
+const courses = [
+  {
+    name: 'Python for Beginners',
+    description: 'Learn the basics of programming in Python.',
+    photo: 'https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010136.jpg?w=900&t=st=1692313887~exp=1692314487~hmac=f21b1a71ff6c57825e46130e37098a5fdc163216b68567791dc48bf7c3fcb4ca',
+    price: 35.99,
+    teacher: 'Martin Suarez'
+  },
+  {
+    name: 'Advanced Digital Photography',
+    description: 'Enhance your skills in digital photography and post-processing.',
+    photo: 'https://img.freepik.com/free-photo/professional-camera-blurred_169016-10249.jpg?w=900&t=st=1692313947~exp=1692314547~hmac=8ed5b07b946b9dda276e7cca5c98345681520a52deb41679d08d123422e97d2e',
+    price: 75.0,
+    teacher: 'Ana Smith'
+  },
+  {
+    name: 'Python for Beginners',
+    description: 'Learn the basics of programming in Python.',
+    photo: 'https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010136.jpg?w=900&t=st=1692313887~exp=1692314487~hmac=f21b1a71ff6c57825e46130e37098a5fdc163216b68567791dc48bf7c3fcb4ca',
+    price: 35.99,
+    teacher: 'Martin Suarez'
+  },
+  {
+    name: 'Python for Beginners',
+    description: 'Learn the basics of programming in Python.',
+    photo: 'https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010136.jpg?w=900&t=st=1692313887~exp=1692314487~hmac=f21b1a71ff6c57825e46130e37098a5fdc163216b68567791dc48bf7c3fcb4ca',
+    price: 35.99,
+    teacher: 'Martin Suarez'
+  },
+  {
+    name: 'Advanced Digital Photography',
+    description: 'Enhance your skills in digital photography and post-processing.',
+    photo: 'https://img.freepik.com/free-photo/professional-camera-blurred_169016-10249.jpg?w=900&t=st=1692313947~exp=1692314547~hmac=8ed5b07b946b9dda276e7cca5c98345681520a52deb41679d08d123422e97d2e',
+    price: 75.0,
+    teacher: 'Ana Smith'
+  },
+  {
+    name: 'Python for Beginners',
+    description: 'Learn the basics of programming in Python.',
+    photo: 'https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010136.jpg?w=900&t=st=1692313887~exp=1692314487~hmac=f21b1a71ff6c57825e46130e37098a5fdc163216b68567791dc48bf7c3fcb4ca',
+    price: 35.99,
+    teacher: 'Martin Suarez'
+  }
+];
+
 const Courses = () => {
+  const [state] = useState(courses);
+
   return (
-    <div>Courses</div>
+    <div class="pad">
+      <div>
+        <div className="container-title">
+          <h2 className="text_bold">LATEST COURSES</h2>
+          <span className="courses-span">Please select a course</span>
+          <span className="courses-span">..........................</span>
+        </div>
+        <div>
+          <Slider courses={courses}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
