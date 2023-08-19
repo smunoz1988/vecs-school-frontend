a1aa4db9fc02c54553ff401813524f28a3d9292d
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-19 11:25:33 -0500

    add select course input to Reserve form

--- src/routes/Reserve.jsx
+++ src/routes/Reserve.jsx
@@ -1,4 +1,5 @@
 import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 // add logic to get current user and course to reserve
@@ -13,7 +14,22 @@ const Reserve = () => {
   return (
     <>
     <p onClick={() => navigate('/')}>Back</p>
-    <div>RESERVE A COURSE</div>
+    <h3>RESERVE A COURSE</h3>
+    <p>Select a course to reserve</p>
+    <form>
+      <select
+        value={course}
+        onChange={(e) => setCourse(e.target.value)}
+      >
+        <option value="">Select a course</option>
+        {courses.map((course) => (
+          <option key={course} value={course}>
+            {course}
+          </option>
+        ))}
+      </select>
+      <button type="submit">Reserve</button>
+    </form>
   </>
   )
 }
