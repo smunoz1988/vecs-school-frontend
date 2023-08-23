637c22d1aadff9295ff507e7d9aed64add6842c8
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-23 15:27:46 -0500

    add conditional to display courses

--- src/routes/Courses.jsx
+++ src/routes/Courses.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCourses } from "../redux/slices/coursesSlice";
 import Slider from "../components/Slider";
+import Navbar from "../components/Navbar";
 import { courses } from '../components/courseData'
 
 const Courses = () => {
@@ -13,18 +14,22 @@ const Courses = () => {
   }, [dispatch]);
 
   return (
-    <div className="pad">
-      <div>
-        <div className="container-title">
-          <h2 className="text_bold">LATEST COURSES</h2>
-          <span className="courses-span">Please select a course</span>
-          <span className="courses-span">..........................</span>
-        </div>
+    <>
+      <Navbar />
+      <div className="pad">
         <div>
-          <Slider courses={coursesData.courses}/>
+          <div className="container-title">
+            <h2 className="text_bold">LATEST COURSES</h2>
+            <span className="courses-span">Please select a course</span>
+            <span className="courses-span">..........................</span>
+            {coursesData.courses.length <= 0 && <p className="courses-span">No courses to show</p>}
+          </div>
+          <div>
+            <Slider courses={coursesData.courses}/>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
