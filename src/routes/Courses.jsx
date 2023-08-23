bf38bac2a3efec1994be22313874d54170c07685
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-22 21:59:10 -0500

    add selector on courses

--- src/routes/Courses.jsx
+++ src/routes/Courses.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCourses } from "../redux/slices/coursesSlice";
 import Slider from "../components/Slider";
 import { courses } from '../components/courseData'
 
 const Courses = () => {
+  const dispatch = useDispatch();
+  const coursesData = useSelector((state) => state.courses);
+
+  useEffect(() => {
+    dispatch(fetchCourses());
+  }, [dispatch]);
+
   return (
     <div className="pad">
       <div>
@@ -11,7 +21,7 @@ const Courses = () => {
           <span className="courses-span">..........................</span>
         </div>
         <div>
-          <Slider courses={courses}/>
+          <Slider courses={coursesData.courses}/>
         </div>
       </div>
     </div>
