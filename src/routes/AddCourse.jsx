596c437bbee7d29d3fdd8cb9048331f6ee43cb15
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-22 23:10:35 -0500

    set logic to add course form

--- src/routes/AddCourse.jsx
+++ src/routes/AddCourse.jsx
@@ -1,15 +1,35 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { createCourse } from '../redux/slices/coursesSlice';
+
+
 const AddCourse = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [photo, setPhoto] = useState('');
+  const [price, setPrice] = useState('');
+  const [teacher, setTeacher] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    dispatch(createCourse({name, teacher, price, photo, description}));
+    navigate('/Courses')
+  }
+
   return (
     <div className="pad flex flex_col ai_center gb_gray">
       <div className="form-container">
         <h2 className="title">ADD NEW COURSE</h2>
-        <form className="flex flex_col w70 form">
-          <input type="text" name="name" placeholder="Name" />
-          <textarea name="description" rows="5" aria-label="description" placeholder="Description"></textarea>
-          <input type="text" name="photo" placeholder="Photo" />
-          <input type="text" name="price" placeholder="Price" />
-          <input type="text" name="teacher" placeholder="Teacher" />
-          <button className="form-submit" type="button">Add Course</button>
+        <form className="flex flex_col w70 form" onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+          <textarea name="description" rows="5" aria-label="description" placeholder="Description" onChange={(e) => setDescription(e.target.value)}></textarea>
+          <input type="text" name="photo" placeholder="Photo" onChange={(e) => setPhoto(e.target.value)} />
+          <input type="text" name="price" placeholder="Price" onChange={(e) => setPrice(e.target.value)} />
+          <input type="text" name="teacher" placeholder="Teacher" onChange={(e) => setTeacher(e.target.value)} />
+          <button className="form-submit" type="submit">Add Course</button>
         </form>
       </div>
    </div>
