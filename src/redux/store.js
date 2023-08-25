71b03fd2d514150983c8bba4d31136f3c486957b
vvoo21 <vaneolipa@gmail.com>
2023-08-25 10:36:08 -0500

    Add reservationReducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import coursesReducer from "./slices/coursesSlice";
+import reservationsReducer from "./slices/reservationsSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     courses: coursesReducer,
+    reservations: reservationsReducer
   },
 });
 
