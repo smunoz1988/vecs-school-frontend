eb2c8314a93c389fb95e25e382ea5d0cd9ac5097
vvoo21 <vaneolipa@gmail.com>
2023-08-28 15:01:03 -0500

    Refactored the AddCourse component

--- src/routes/AddCourse.jsx
+++ src/routes/AddCourse.jsx
@@ -20,8 +20,6 @@ const AddCourse = () => {
   }
 
   return (
-    <div className="pad flex flex_col ai_center gb_gray">
-      <p onClick={() => navigate('/courses')}>Back</p>
       <div className="form-container">
         <h2 className="title">ADD NEW COURSE</h2>
         <form className="flex flex_col w70 form" onSubmit={handleSubmit}>
@@ -33,7 +31,6 @@ const AddCourse = () => {
           <button className="form-submit" type="submit">Add Course</button>
         </form>
       </div>
-   </div>
   )
 }
 
